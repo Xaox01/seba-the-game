a5c289c6f5f918528cc46ed656b882b9ce6f0bc8
unknown <studiocrystalgames06@gmail.com>
2024-04-25 07:06:46 +0200

    add WSAD

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const mapData = {
   minX: 1,
   maxX: 14,
@@ -19,13 +20,12 @@ const mapData = {
   },
 };
 
-// Options for Player Colors... these are in the same order as our sprite sheet
 const playerColors = ["blue", "red", "orange", "yellow", "green", "purple"];
 
-//Misc Helpers
 function randomFromArray(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
+
 function getKeyString(x, y) {
   return `${x}x${y}`;
 }
@@ -71,7 +71,6 @@ function createName() {
 }
 
 function isSolid(x,y) {
-
   const blockedNextSpace = mapData.blockedSpaces[getKeyString(x, y)];
   return (
     blockedNextSpace ||
@@ -83,7 +82,6 @@ function isSolid(x,y) {
 }
 
 function getRandomSafeSpot() {
-  //We don't look things up by key here, so just return an x/y
   return randomFromArray([
     { x: 1, y: 4 },
     { x: 2, y: 4 },
@@ -111,9 +109,7 @@ function getRandomSafeSpot() {
   ]);
 }
 
-
 (function () {
-
   let playerId;
   let playerRef;
   let players = {};
@@ -125,14 +121,10 @@ function getRandomSafeSpot() {
   const playerNameInput = document.querySelector("#player-name");
   const playerColorButton = document.querySelector("#player-color");
 
-
   function placeCoin() {
     const { x, y } = getRandomSafeSpot();
     const coinRef = firebase.database().ref(`coins/${getKeyString(x, y)}`);
-    coinRef.set({
-      x,
-      y,
-    })
+    coinRef.set({ x, y });
 
     const coinTimeouts = [2000, 3000, 4000, 5000];
     setTimeout(() => {
@@ -143,20 +135,15 @@ function getRandomSafeSpot() {
   function attemptGrabCoin(x, y) {
     const key = getKeyString(x, y);
     if (coins[key]) {
-      // Remove this key from data, then uptick Player's coin count
       firebase.database().ref(`coins/${key}`).remove();
-      playerRef.update({
-        coins: players[playerId].coins + 1,
-      })
+      playerRef.update({ coins: players[playerId].coins + 1 });
     }
   }
 
-
-  function handleArrowPress(xChange=0, yChange=0) {
+  function handleKeyPress(xChange=0, yChange=0) {
     const newX = players[playerId].x + xChange;
     const newY = players[playerId].y + yChange;
     if (!isSolid(newX, newY)) {
-      //move to the next space
       players[playerId].x = newX;
       players[playerId].y = newY;
       if (xChange === 1) {
@@ -171,22 +158,19 @@ function getRandomSafeSpot() {
   }
 
   function initGame() {
-
-    new KeyPressListener("ArrowUp", () => handleArrowPress(0, -1))
-    new KeyPressListener("ArrowDown", () => handleArrowPress(0, 1))
-    new KeyPressListener("ArrowLeft", () => handleArrowPress(-1, 0))
-    new KeyPressListener("ArrowRight", () => handleArrowPress(1, 0))
+    new KeyPressListener("w", () => handleKeyPress(0, -1));
+    new KeyPressListener("s", () => handleKeyPress(0, 1));
+    new KeyPressListener("a", () => handleKeyPress(-1, 0));
+    new KeyPressListener("d", () => handleKeyPress(1, 0));
 
     const allPlayersRef = firebase.database().ref(`players`);
     const allCoinsRef = firebase.database().ref(`coins`);
 
     allPlayersRef.on("value", (snapshot) => {
-      //Fires whenever a change occurs
       players = snapshot.val() || {};
       Object.keys(players).forEach((key) => {
         const characterState = players[key];
         let el = playerElements[key];
-        // Now update the DOM
         el.querySelector(".Character_name").innerText = characterState.name;
         el.querySelector(".Character_coins").innerText = characterState.coins;
         el.setAttribute("data-color", characterState.color);
@@ -194,10 +178,10 @@ function getRandomSafeSpot() {
         const left = 16 * characterState.x + "px";
         const top = 16 * characterState.y - 4 + "px";
         el.style.transform = `translate3d(${left}, ${top}, 0)`;
-      })
-    })
+      });
+    });
+
     allPlayersRef.on("child_added", (snapshot) => {
-      //Fires whenever a new node is added the tree
       const addedPlayer = snapshot.val();
       const characterElement = document.createElement("div");
       characterElement.classList.add("Character", "grid-cell");
@@ -214,8 +198,6 @@ function getRandomSafeSpot() {
         <div class="Character_you-arrow"></div>
       `);
       playerElements[addedPlayer.id] = characterElement;
-
-      //Fill in some initial state
       characterElement.querySelector(".Character_name").innerText = addedPlayer.name;
       characterElement.querySelector(".Character_coins").innerText = addedPlayer.coins;
       characterElement.setAttribute("data-color", addedPlayer.color);
@@ -224,90 +206,64 @@ function getRandomSafeSpot() {
       const top = 16 * addedPlayer.y - 4 + "px";
       characterElement.style.transform = `translate3d(${left}, ${top}, 0)`;
       gameContainer.appendChild(characterElement);
-    })
+    });
 
-
-    //Remove character DOM element after they leave
     allPlayersRef.on("child_removed", (snapshot) => {
       const removedKey = snapshot.val().id;
       gameContainer.removeChild(playerElements[removedKey]);
       delete playerElements[removedKey];
-    })
+    });
 
-
-    //New - not in the video!
-    //This block will remove coins from local state when Firebase `coins` value updates
     allCoinsRef.on("value", (snapshot) => {
       coins = snapshot.val() || {};
     });
-    //
 
     allCoinsRef.on("child_added", (snapshot) => {
       const coin = snapshot.val();
       const key = getKeyString(coin.x, coin.y);
       coins[key] = true;
-
-      // Create the DOM Element
       const coinElement = document.createElement("div");
       coinElement.classList.add("Coin", "grid-cell");
       coinElement.innerHTML = `
         <div class="Coin_shadow grid-cell"></div>
         <div class="Coin_sprite grid-cell"></div>
       `;
-
-      // Position the Element
       const left = 16 * coin.x + "px";
       const top = 16 * coin.y - 4 + "px";
       coinElement.style.transform = `translate3d(${left}, ${top}, 0)`;
-
-      // Keep a reference for removal later and add to DOM
       coinElements[key] = coinElement;
       gameContainer.appendChild(coinElement);
-    })
+    });
+
     allCoinsRef.on("child_removed", (snapshot) => {
-      const {x,y} = snapshot.val();
-      const keyToRemove = getKeyString(x,y);
-      gameContainer.removeChild( coinElements[keyToRemove] );
+      const { x, y } = snapshot.val();
+      const keyToRemove = getKeyString(x, y);
+      gameContainer.removeChild(coinElements[keyToRemove]);
       delete coinElements[keyToRemove];
-    })
+    });
 
-
-    //Updates player name with text input
     playerNameInput.addEventListener("change", (e) => {
       const newName = e.target.value || createName();
       playerNameInput.value = newName;
-      playerRef.update({
-        name: newName
-      })
-    })
+      playerRef.update({ name: newName });
+    });
 
-    //Update player color on button click
     playerColorButton.addEventListener("click", () => {
       const mySkinIndex = playerColors.indexOf(players[playerId].color);
       const nextColor = playerColors[mySkinIndex + 1] || playerColors[0];
-      playerRef.update({
-        color: nextColor
-      })
-    })
+      playerRef.update({ color: nextColor });
+    });
 
-    //Place my first coin
     placeCoin();
-
   }
 
   firebase.auth().onAuthStateChanged((user) => {
-    console.log(user)
     if (user) {
-      //You're logged in!
       playerId = user.uid;
       playerRef = firebase.database().ref(`players/${playerId}`);
-
       const name = createName();
       playerNameInput.value = name;
-
-      const {x, y} = getRandomSafeSpot();
-
-
+      const { x, y } = getRandomSafeSpot();
       playerRef.set({
         id: playerId,
         name,
@@ -316,24 +272,18 @@ function getRandomSafeSpot() {
         x,
         y,
         coins: 0,
-      })
-
-      //Remove me from Firebase when I diconnect
+      });
       playerRef.onDisconnect().remove();
-
-      //Begin the game now that we are signed in
       initGame();
     } else {
       //You're logged out.
     }
-  })
+  });
 
   firebase.auth().signInAnonymously().catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
-    // ...
     console.log(errorCode, errorMessage);
   });
-
-
 })();
+
